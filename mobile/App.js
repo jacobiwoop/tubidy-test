4a12a51ce51647695ec0c925b8fbb00606044436
jacobiwoop <jacobiwoop@gmail.com>
2026-05-01 21:12:26 +0200

    fix: adjust mini player position to avoid overlap with bottom bar

--- mobile/App.js
+++ mobile/App.js
@@ -314,7 +314,7 @@ export default function App() {
           <TouchableOpacity 
             activeOpacity={0.9}
             onPress={() => setShowFullPlayer(true)}
-            style={[styles.miniPlayer, { bottom: 65 }]}
+            style={styles.miniPlayer}
           >
             {/* Pas de dégradé, on utilise le style de fond du miniPlayer */}
             <Image 
