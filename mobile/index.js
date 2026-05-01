df8359f5e3db045a390c3cc37aa4345a894aefa4
jacobiwoop <jacobiwoop@gmail.com>
2026-05-01 05:45:53 +0200

    modified:   mobile/app.json 	modified:   mobile/index.js 	modified:   mobile/package.json

--- mobile/index.js
+++ mobile/index.js
@@ -1,16 +1,6 @@
 import { registerRootComponent } from 'expo';
-import audioModule from './src/utils/audioFactory';
 import App from './App';
 
-const { TrackPlayer, isMock } = audioModule;
-
-if (!isMock && TrackPlayer.registerPlaybackService) {
-  try {
-    const { playbackService } = require('./src/services/playbackService');
-    TrackPlayer.registerPlaybackService(() => playbackService);
-  } catch (e) {
-    console.warn('Playback service registration failed', e);
-  }
-}
-
+// registerRootComponent s'assure que l'application est lancée correctement
+// que ce soit sur Expo Go ou dans un build natif.
 registerRootComponent(App);
