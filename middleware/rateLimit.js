be6863192a217ae05cdfd3c8b9a9c1b2c2f273a8
jacobiwoop <jacobiwoop@gmail.com>
2026-05-04 12:28:18 +0200

    Cleanup: Move mobile project to root and remove non-mobile files

--- middleware/rateLimit.js
+++
@@ -1,4 +0,0 @@
-const rateLimit = require('express-rate-limit');
-const defaultLimiter = rateLimit({ windowMs: 15*60*1000, max: 100, message: { error: 'Too many requests.' } });
-const searchLimiter  = rateLimit({ windowMs: 60*1000,    max: 20,  message: { error: 'Search rate limit exceeded.' } });
-module.exports = { defaultLimiter, searchLimiter };
