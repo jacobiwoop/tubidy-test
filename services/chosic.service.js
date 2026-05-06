891da81ba109e04e55c06c5013e1c342021fae6a
jacobiwoop <jacobiwoop@gmail.com>
2026-05-06 23:23:23 +0200

    fix: hardcode chosic cookie to bypass env vars

--- services/chosic.service.js
+++ services/chosic.service.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 
 // NOTE: Le cookie de session est nécessaire pour les recommandations.
 // Il peut expirer. Idéalement, il devrait être dans ton fichier .env
-const CHOSIC_COOKIE = process.env.CHOSIC_COOKIE || "pll_language=en; r_34874064=1778095732%7C85033bbc5b3d58c4%7C74c1fc442748392b381f3414e9ff5f8bd547a5ed1f10efa63515bfae7e3a6611";
+const CHOSIC_COOKIE = "pll_language=en; r_34874064=1778095732%7C85033bbc5b3d58c4%7C74c1fc442748392b381f3414e9ff5f8bd547a5ed1f10efa63515bfae7e3a6611";
 
 const COMMON_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36',
